5fdf577a25f617152aa6bf8a33ac40e19004a806
Jake Palmer <ijambro@gmail.com>
2024-05-13 22:26:55 -0400

    Create initial Node project with Typescript and Express

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(
+    "Express + TypeScript Server is running 🧑🏻‍💻 and restarting on file changes 👀"
+  );
+});
+
+app.listen(port, () => {
+  console.log(`[server]: 🔥 Server is running at http://localhost:${port}`);
+});
